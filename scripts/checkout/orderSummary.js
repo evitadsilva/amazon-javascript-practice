a24068e74a3cb945f9743ffc00ad07c187f9f538
dsevita <evita.dsilva03@gmail.com>
2024-11-28 02:40:58 +0530

    Make payment summary interactive

--- scripts/checkout/orderSummary.js
+++ scripts/checkout/orderSummary.js
@@ -4,12 +4,7 @@ import { formatCurrency } from "../utils/money.js";
 import {hello} from 'https://unpkg.com/supersimpledev@1.0.1/hello.esm.js';
 import dayjs from 'https://unpkg.com/dayjs@1.11.10/esm/index.js';
 import {deliveryOptions, getDeliveryOption} from '../../data/deliveryOptions.js';
-
-hello();
-
-const today = dayjs();
-const deliveryDate = today.add(7, 'days');
-console.log(deliveryDate.format('dddd, MMMM D'));
+import { renderPaymentSummary } from "./paymentSummary.js";
 
 export function renderOrderSummary() {
   let cartSummaryHtml = '';
@@ -144,6 +139,7 @@ export function renderOrderSummary() {
         container.remove();
 
         updateCartQuantity();
+        renderPaymentSummary();
       });
     });
 
@@ -211,6 +207,7 @@ export function renderOrderSummary() {
           const {productId, deliveryOptionId} = element.dataset;
           updateDeliveryOption(productId, deliveryOptionId);
           renderOrderSummary(); //a function can call / re-run itself
+          renderPaymentSummary();
         });
       });
 }
