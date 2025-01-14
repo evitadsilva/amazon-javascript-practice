1660791ff427066e8eaaf58539aa415d00a31673
dsevita <evita.dsilva03@gmail.com>
2025-01-14 21:42:24 +0530

    Remove default tests and add money tests

--- tests/moneyTest.js
+++ tests/moneyTest.js
@@ -2,7 +2,7 @@ import { formatCurrency } from '../scripts/utils/money.js';
 
 console.log('test suite: formatCurrency');
 
-console.log('converts cents into dolars');
+console.log('converts cents into dollars');
 
 if (formatCurrency(2095) === '20.95')  {
   console.log('passed');
