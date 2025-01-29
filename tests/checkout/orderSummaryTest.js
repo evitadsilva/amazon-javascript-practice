407eda057cede78636dd91a6193be66f0e01a724
dsevita <evita.dsilva03@gmail.com>
2025-01-29 20:16:36 +0530

    Added more tests for formatCurrency() and addToCart()

--- tests/checkout/orderSummaryTest.js
+++ tests/checkout/orderSummaryTest.js
@@ -31,6 +31,10 @@ describe('test suite: renderOrderSummary', () => {
         renderOrderSummary();
   });
 
+  afterEach(() => {
+    document.querySelector('.js-test-container').innerHTML = '';
+  });
+
   it('displays the cart', () => {
     expect(
       document.querySelectorAll('.js-cart-item-container').length
@@ -41,8 +45,18 @@ describe('test suite: renderOrderSummary', () => {
     expect(
       document.querySelector(`.js-product-quantity-${productId2}`).innerText
     ).toContain('Quantity: 1');
-
-    document.querySelector('.js-test-container').innerHTML = '';
+    expect(
+      document.querySelector(`.js-product-name-${productId1}`).innerText
+    ).toEqual('Black and Gray Athletic Cotton Socks - 6 Pairs');
+    expect(
+      document.querySelector(`.js-product-name-${productId2}`).innerText
+    ).toEqual('Intermediate Size Basketball');
+    expect(
+      document.querySelector(`.js-product-price-${productId1}`).innerText
+    ).toEqual('$10.90');
+    expect(
+      document.querySelector(`.js-product-price-${productId2}`).innerText
+    ).toEqual('$20.95');
 
   });
 
@@ -60,12 +74,32 @@ describe('test suite: renderOrderSummary', () => {
           document.querySelector(`.js-cart-item-container-${productId2}`)
         ).not.toEqual(null);
 
+        expect(document.querySelector(`.js-product-name-${productId2}`).innerText
+        ).toEqual('Intermediate Size Basketball');
+
+        expect(
+          document.querySelector(`.js-product-price-${productId2}`).innerText
+        ).toEqual('$20.95');
+
         expect(cart.length).toEqual(1);
 
         expect(cart[0].productId).toEqual(productId2);
 
-        document.querySelector('.js-test-container').innerHTML = '';
-
   });
 
-});
+  it('updates the delivery options', () => {
+    document.querySelector(`.js-delivery-option-${productId1}-3`).click();
+
+    expect( 
+      document.querySelector(`.js-delivery-option-input-${productId1}-3`).checked
+    ).toEqual(true);
+
+    expect(cart.length).toEqual(2);
+
+    expect(cart[0].productId).toEqual(productId1);
+
+    expect(cart[0].deliveryOptionId).toEqual('3');
+    
+    });
+
+  });
--- tests/utils/moneyTest.js
+++ tests/utils/moneyTest.js
@@ -11,6 +11,11 @@ describe('test suite: formatCurrency', () => {
 
   it('rounds up to the nearest cent', () => {
     expect(formatCurrency(2000.5)).toEqual('20.01');
+    expect(formatCurrency(2000.4)).toEqual('20.00');
+  });
+
+  it('works with a negative number', () => {
+    expect(formatCurrency(-200)).toEqual('-2.00');
   });
 
 })
