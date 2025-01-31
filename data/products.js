d1eafad1ba3dba9d18d857739f7b0436b1525b38
dsevita <evita.dsilva03@gmail.com>
2025-01-31 17:22:30 +0530

    Convert loadProducts from XHR to fetch

--- data/products.js
+++ data/products.js
@@ -116,6 +116,34 @@ object3.method();
 
 export let products = [];
 
+export function loadProductsFetch() {
+  const promise = fetch(
+    'http://supersimplebackend.dev/products'
+  ).then((response) => {
+    return response.json();
+  }).then((productData) => {
+    products = productData.map((productDetails) => {
+      if(productDetails.type === 'clothing') {
+        return new Clothing(productDetails);
+      }
+      else if(productDetails.type === 'appliance'){
+        return new Appliance(productDetails);
+      }
+      return new Product(productDetails);
+    });
+
+    console.log('load products');
+  });
+
+  return promise;
+}
+
+/*
+loadProductsFetch().then(() => {
+  console.log('next step');
+});
+*/
+
 export function loadProducts(fun) {
   const xhr = new XMLHttpRequest();
 
